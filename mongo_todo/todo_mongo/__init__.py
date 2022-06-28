1fd07661c5e44c0a2606830c86ba05a432d4f7d4
Giorgio Morello <99151447+GiorgioMorello@users.noreply.github.com>
2022-06-28 09:19:59 -0300

    Update __init__.py

--- mongo_todo/todo_mongo/__init__.py
+++ mongo_todo/todo_mongo/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, url_for, redirect, request
 from models import mongo
-from bson.objectid import ObjectId
+from bson.objectid import ObjectId # The ObjectID will take the ID and convert it to the object ID that mongo can work
 
 
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb+srv://todo2:amonintendo5@cluster0.us7rg.mongodb.net/mydb?retryWrites=true&w=majority'
+app.config['MONGO_URI'] = 'mongodb+srv://todo2:<password>@cluster0.us7rg.mongodb.net/mydb?retryWrites=true&w=majority'
 mongo.init_app(app)
 
 
@@ -13,15 +13,15 @@ mongo.init_app(app)
 @app.route('/', methods=['POST', 'GET'])
 def home():
     todos_collection = mongo.db.todos
-    todos = todos_collection.find()
+    todos = todos_collection.find() # Will get all the items inside the DB
     return render_template('index.html', todos=todos)
 
 
 @app.route('/add_todo', methods=['POST', 'GET'])
 def add():
-    todos_collection = mongo.db.todos
+    todos_collection = mongo.db.todos # Will create our collections
     todo_item = request.form.get('new_todo')
-    todos_collection.insert_one({'text' : todo_item, 'complete' : False})
+    todos_collection.insert_one({'text' : todo_item, 'complete' : False}) # Will create our documents and fields
     return redirect(url_for('home'))
 
 
@@ -42,6 +42,8 @@ def not_completed(todo_id):
 @app.route('/delete-completed')
 def delete():
     todos_collection = mongo.db.todos
+    
+    # Will delete all items that have the complete field equal to True
     todos_collection.delete_many({'complete' : True})
     return redirect(url_for('home'))
 
@@ -49,6 +51,7 @@ def delete():
 @app.route('/delete-all')
 def delete_all():
     todos_collection = mongo.db.todos
+    # If you pass an empty dictionary, it will represent an empty query, which means all items
     todos_collection.delete_many({})
     return redirect(url_for('home'))
 
